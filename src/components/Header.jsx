ac91562244702012aace790117f31398f1115727
Ednario Andrade <joseednario637@gmail.com>
2022-02-27 22:59:30 -0300

    logo-atualizada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return ( 
     <header className={styles.header} id="header">
       <nav className={`${styles.nav} ${styles.container}`}>
-        <a className={styles.logo} href="#">Ednario<span>Andrade</span>.</a>
+        <a className={styles.logo} href="#">Ednario<span href="#"> Andrade</span></a>
       </nav>
     </header>
   )
