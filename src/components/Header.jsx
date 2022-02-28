be514ccf8514972a95956d2e5890b7a024ee15c6
Ednario Andrade <joseednario637@gmail.com>
2022-02-27 22:05:37 -0300

    menu-removido-bugs_causa:encontrados

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,6 @@ export default function Header() {
     <header className={styles.header} id="header">
       <nav className={`${styles.nav} ${styles.container}`}>
         <a className={styles.logo} href="#">Ednario<span>Andrade</span>.</a>
-        <div className={styles.menu}>
-          <ul className={styles.grid}>
-            <li><a className={styles.title} href="#home">Início</a></li>
-            <li><a className={styles.title} href="#about">Sobre</a></li>
-            <li><a className={styles.title} href="#contact">Redes Sociais</a></li>
-          </ul>
-        </div>
-        <div className={`${styles.toggle} ${styles.iconMenu}`}><FiMenu/></div>
-        <div className={`${styles.toggle} ${styles.iconClose}`}><FiX/></div>
       </nav>
     </header>
   )
